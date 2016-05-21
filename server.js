cb1abe2aa785b134a2849b78974d3978c50ceda5
tomkoker <teddy.koker@gmail.com>
2016-05-21 17:35:34 -0400

    more stuff

--- server.js
+++ server.js
@@ -20,10 +20,18 @@ io.on('connection' , function(socket){
 
   socket.on('disconnect', function(){
     console.log('Disconnection.');
+    disconnect(socket);
   })
 
   socket.on('new mobile', function(data){
-    io.to(games[0].display).emit('new mobile', data);
+    if(typeof games[0] == 'undefined'){
+      console.log("no display");
+      io.to(socket.id).emit('no display');
+    } else {
+      console.log("new mobile");
+      newMobile(socket);
+      io.to(games[0].display).emit('new mobile', data);
+    }
   });
 
   socket.on('new display', function(data){
@@ -41,7 +49,21 @@ function newDisplay(socket){
   games[0] = {
     display: socket.id,
   };
-  games.players = {};
+  games[0].players = {};
+}
+function disconnect(socket){
+  for(var i in games){
+    if (games[i].display == socket.id){
+      console.log("display quit");
+    } else {
+      for(var player in games[i].players){
+        if(player == socket.id){
+          console.log("player quit");
+          io.to(games[i].display).emit('player left', {id: socket.id})
+        }
+      }
+    }
+  }
 }
 
 function newMobile(socket){
