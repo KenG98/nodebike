e5fb1df5c7df0dbb0c76a04aceac159394836c47
tomkoker <teddy.koker@gmail.com>
2016-05-21 18:41:13 -0400

    update color

--- server.js
+++ server.js
@@ -37,6 +37,10 @@ io.on('connection' , function(socket){
   socket.on('start game', function(){
     io.to('room ' + '0').emit('start game');
   });
+  socket.on('update color', function(data){
+    io.to('room ' + '0').emit('update color', data);
+  });
+
   socket.on('update', function(data){
     io.to(games[0].display).emit('update', data);
   });
