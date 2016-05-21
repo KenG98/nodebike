15f38c3e17c09e88013d6a4228c115f49d1843d2
Ken Garber <kengarber98@gmail.com>
2016-05-21 13:51:12 -0400

    update some JS

---
+++ public/display/index.js
@@ -0,0 +1,41 @@
+var socket = io('/display');
+
+gameData = {};
+
+function waitroomHTML(){
+	var h = '<p>Waiting for game to start</p> \n ';
+	h += '<p>Players:</p> \n';
+	for(p in gameData.players){
+		h += p.id + '\n';
+	}
+	h += '<button onclick="startGame()">Start Game</button>';
+}
+
+$('#page').html(waitroomHTML());
+
+socket.on('player joined', function(data){
+	gameData.players = data;
+	$('#page').html(waitroomHTML());
+});
+
+socket.on('update game data', function(data){
+	gameData = data;
+});
+
+function startGame(){
+	socket.emit('start game');
+	$('#page').html(gameroomHTML());
+	canvas = $('#gamescreen')[0];
+	canvas.width = $('#page').width();
+	canvas.height = $('#page').height();
+	canv = canvas.getContext("2d");
+	setInterval(renderCanvas, 34);
+}
+
+function gameroomHTML(){
+	return '<canvas id="gamescreen"></canvas>';
+}
+
+function renderCanvas(){
+	// draw the screen
+}
