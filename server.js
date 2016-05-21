86634c3472f7495ab5f2af7ab3faf85c2f59a040
tomkoker <teddy.koker@gmail.com>
2016-05-21 19:00:49 -0400

    console logs

--- server.js
+++ server.js
@@ -20,33 +20,36 @@ io.on('connection' , function(socket){
 
   socket.on('new mobile', function(data){
     if(typeof games[0] == 'undefined'){
-      console.log("no display");
+      console.log("No display: " + socket.id);
       io.to(socket.id).emit('no display');
     } else {
-      console.log("new mobile");
+      console.log("New mobile: " + socket.id);
       newMobile(socket);
       io.to(games[0].display).emit('new mobile', data);
     }
   });
 
   socket.on('new display', function(data){
-    console.log('new display');
+    console.log('New display: ' + socket.id);
     newDisplay(socket);
   });
 
   socket.on('start game', function(){
+    console.log('Start Game: ' + socket.id)
     io.to('room ' + '0').emit('start game');
   });
   socket.on('update color', function(data){
+    console.log('Update color: ' + id);
     io.to(data.id).emit('update color', data);
   });
 
   socket.on('update', function(data){
+    console.log('Update: ' + socket.id)
     io.to(games[0].display).emit('update', data);
   });
 
   socket.on('disconnect', function(){
-    console.log('Disconnection.');
+    console.log('Disconnect: ' socket.id);
     disconnect(socket);
   });
 });
@@ -61,11 +64,11 @@ function newDisplay(socket){
 function disconnect(socket){
   for(var i in games){
     if (games[i].display == socket.id){
-      console.log("display quit");
+      console.log("Display quit: " + socket.id);
     } else {
       for(var player in games[i].players){
         if(player == socket.id){
-          console.log("player quit");
+          console.log("Player quit: " + socket.id);
           io.to(games[i].display).emit('player left', {id: socket.id})
         }
       }
